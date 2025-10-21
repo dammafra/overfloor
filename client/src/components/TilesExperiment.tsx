f1f809ff7b9222349b424eef58f6ccbeb229fbd5
Francesco Dammacco <francescodammacco@live.it>
2025-10-21 21:50:10 +0200

    Tiles animation WIP

---
+++ client/src/components/TilesExperiment.tsx
@@ -0,0 +1,38 @@
+import { useSprings } from '@react-spring/three'
+import { Float } from '@react-three/drei'
+import { spiralPositionLobby } from '@utils'
+import { type EulerTuple, type Vector3Tuple } from 'three'
+import { useRoute } from 'wouter'
+import { Tile } from './Tile'
+
+export function Temp() {
+  const [matchLobby] = useRoute(/lobby/)
+  // const ref = useRef<Mesh>(null)
+
+  // useFrame((_, delta) => {
+  //   if (!ref.current) return
+  //   ref.current.rotation.x += delta * 0.5
+  //   ref.current.rotation.y += delta * 0.25
+  //   ref.current.rotation.z += delta * 0.125
+  // })
+
+  const [springs] = useSprings<{ position: Vector3Tuple; rotation: EulerTuple | undefined }>(
+    10,
+    i => ({
+      position: matchLobby ? spiralPositionLobby(i, 3) : [i - 5, 0, i * 2],
+      rotation: matchLobby ? [1, 2, 3] : undefined,
+      config: { mass: 1, tension: 120, friction: 14 },
+    }),
+    [matchLobby],
+  )
+
+  return springs.map((spring, i) => (
+    <Float
+      key={`menu-tile-${i}`}
+      floatIntensity={matchLobby ? 0 : 5}
+      rotationIntensity={matchLobby ? 0 : 10}
+    >
+      <Tile position={spring.position} rotation={spring.rotation} />
+    </Float>
+  ))
+}
--- client/src/utils/spiral-position.ts
+++ client/src/utils/spiral-position.ts
@@ -1,21 +1,19 @@
 import { MathUtils, type Vector3Tuple } from 'three'
 
 // TODO*: move to server or find a better way to implement `CameraRig`
-export function spiralPositionLobby(index: number): Vector3Tuple {
-  const r = 1.5
+export function spiralPositionLobby(index: number, radius: number = 1.5): Vector3Tuple {
   const angle = MathUtils.degToRad(index * 30) // adjust spacing
-  const x = r * Math.sin(angle)
-  const y = r * Math.cos(angle)
+  const x = radius * Math.sin(angle)
+  const y = radius * Math.cos(angle)
   const z = index * 0.5 // step outward
   return [x, y, z]
 }
 
 // TODO**: improve, duplicate
-export function spiralPositionGame(index: number): Vector3Tuple {
-  const r = 1.5
+export function spiralPositionGame(index: number, radius: number = 1.5): Vector3Tuple {
   const angle = MathUtils.degToRad(index * 60) // spacing
-  const x = r * Math.cos(angle)
-  const z = r * Math.sin(angle)
+  const x = radius * Math.cos(angle)
+  const z = radius * Math.sin(angle)
   const y = 4 // constant height
   return [x, y, z]
 }
