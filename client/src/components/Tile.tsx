3d22e8740f698ca64872221c9d40a3613666cf1c
Francesco Dammacco <francescodammacco@live.it>
2025-10-26 16:06:36 +0100

    Minor UI improvements

--- client/src/components/Tile.tsx
+++ client/src/components/Tile.tsx
@@ -1,5 +1,5 @@
 import { a } from '@react-spring/three'
-import { RoundedBoxGeometry } from '@react-three/drei'
+import { Float, RoundedBoxGeometry } from '@react-three/drei'
 import type { JSX } from 'react'
 import { forwardRef } from 'react'
 import type { ColorRepresentation, Mesh } from 'three'
@@ -12,10 +12,12 @@ type TileProps = JSX.IntrinsicElements['mesh'] & {
 export const Tile = a(
   forwardRef<Mesh, TileProps>(({ unit = 1, color, ...props }, ref) => {
     return (
-      <mesh ref={ref} {...props}>
-        <RoundedBoxGeometry args={[unit, 0.25, unit]} radius={0.1} />
-        <meshStandardMaterial color={color} transparent opacity={0.5} />
-      </mesh>
+      <Float floatIntensity={0.5} speed={10} rotationIntensity={0}>
+        <mesh ref={ref} {...props}>
+          <RoundedBoxGeometry args={[unit, 0.25, unit]} radius={0.1} />
+          <meshStandardMaterial color={color} transparent opacity={0.5} />
+        </mesh>
+      </Float>
     )
   }),
 )
--- client/src/components/pages/MainMenu.tsx
+++ client/src/components/pages/MainMenu.tsx
@@ -7,11 +7,13 @@ export function MainMenu() {
       <ul className="flex flex-col items-center gap-4">
         <li className="flex w-full">
           <Link className="button w-full" href="/new">
+            <span className="icon-[mdi--plus]" />
             Create Room
           </Link>
         </li>
         <li className="flex w-full">
           <Link className="button w-full" href="/join">
+            <span className="icon-[mdi--arrow-right]" />
             Join Room
           </Link>
         </li>
@@ -20,6 +22,7 @@ export function MainMenu() {
             className="button warning w-full"
             href={`/new/lobby/${btoa(JSON.stringify({ id: uuid(), username: uuid(), training: true, countdown: 3 }))}`}
           >
+            <span className="icon-[mdi--gym]" />
             Training
           </Link>
         </li>
