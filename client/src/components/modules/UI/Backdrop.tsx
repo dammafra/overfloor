0eca05ecb1eaa89893bb2dc7c777eca79d032ec1
Francesco Dammacco <francescodammacco@live.it>
2025-11-06 00:15:04 +0100

    Revert backdrop modal close

--- client/src/components/modules/UI/Backdrop.tsx
+++ client/src/components/modules/UI/Backdrop.tsx
@@ -1,10 +1,8 @@
 import { a, useSpring } from '@react-spring/three'
 import { MathUtils } from 'three'
-import { useLocation, useRoute } from 'wouter'
+import { useRoute } from 'wouter'
 
 export function Backdrop() {
-  const [, navigate] = useLocation()
-
   const [matchNew] = useRoute('/new')
   const [matchJoin] = useRoute('/join/:id?')
   const [matchCredits] = useRoute('/credits')
@@ -13,18 +11,7 @@ export function Backdrop() {
   const { opacity } = useSpring({ opacity: match ? 0.8 : 0 })
 
   return (
-    <mesh
-      onClick={e => {
-        if (!match) return
-        navigate('/')
-        e.stopPropagation()
-      }}
-      onPointerOver={e => e.stopPropagation()}
-      renderOrder={2}
-      rotation-x={MathUtils.degToRad(-90)}
-      scale={100}
-      position-y={2}
-    >
+    <mesh renderOrder={2} rotation-x={MathUtils.degToRad(-90)} scale={100} position-y={2}>
       <planeGeometry />
       <a.meshBasicMaterial color="black" transparent opacity={opacity} />
     </mesh>
