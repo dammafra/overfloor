c56d1d8015737188768f59003badc45d300f21d1
Francesco Dammacco <francescodammacco@live.it>
2025-11-10 00:24:15 +0100

    Cleanup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl'
 import restart from 'vite-plugin-restart'
@@ -21,10 +20,5 @@ export default defineConfig({
   build: {
     emptyOutDir: true, // Empty the folder first
     sourcemap: true, // Add sourcemap
-  },
-  resolve: {
-    alias: {
-      '@shaders': path.resolve(__dirname, 'src/shaders'),
-    },
   },
 })
